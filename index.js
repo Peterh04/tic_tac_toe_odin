5638c0b09471065c378cc162df4522d65b6ef134
Peter Heri <heripeter725@gmail.com>
2024-10-24 11:17:21 +0300

    Improved the win functionality
    Prevented futher move once the winner is found

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
+
+const buttonCell = document.querySelectorAll('.cell');
+const roundMessage= document.querySelector(`#roundMessage`);
+
+
+
+
+
+
+
 let gameBoard = (function(){
     const rows = 3;
     const columns = 3;
-
+    
     let board = [];
-
+    
     //0, 1,2
-
+    
     const winningCombinations = [
         //Horzontal win
         [[0, 0], [0, 1],[0, 2]],
@@ -15,12 +25,12 @@ let gameBoard = (function(){
         [[0, 0], [1, 0], [2, 0]],
         [[0, 1,], [1, 1], [2, 1]],
         [[0, 2], [1, 2], [2, 2]],
-
+        
         //Diagnol win
         [[0, 0], [1,1], [2,2]],
         [[0,2], [1, 1], [2,0]],
     ]
-
+    
     for(let i = 0; i < 3; i++){
         board[i] = [];
         for(let j = 0; j < 3 ; j++){
@@ -29,127 +39,144 @@ let gameBoard = (function(){
     }
 
     let getBoard = () => board;
-
+    
     let dropToken = (row, column, token) => {
-       if(board[row][column] === null){
-        board[row][column] = token;
-        console.log('dropped')
-        game.switchPlayer()
-       }
-       console.log('Occupied')
-       return;
-     }
-
-
+        if(board[row][column] === null){
+            board[row][column] = token;
+            
+            console.log('dropped')
+           
+        }
+        console.log('Occupied')
+        return;
+    }
+    
+   
     
     return { getBoard, dropToken, winningCombinations }
 })();
 
 
-
 function gameController(playerOne = 'Player One', playerTwo = 'Player Two'){
     let board = gameBoard.getBoard();
     let gameActive = true;
-
+    
     const Players = [
         {
             name : playerOne,
             token : 'X'
         },
-
+        
         {
             name : playerTwo,
             token : 'O',
         }
     ]
-
+    
     let currentPlayer = Players[0];
-
+    
     const switchPlayer = () => {
         currentPlayer = currentPlayer === Players[0] ? Players[1] : Players[0]
     }
-
+    
     const getCurrentPlayer = () => currentPlayer;
-
+    
     const checkWinner = (token) => {
         
-
+        
         gameBoard.winningCombinations.forEach((arr)=>{
-          const isWinningCombination =   arr.every((cell)=>{
-               return board[cell[0]][cell[1]] === token
+            const isWinningCombination = arr.every((cell)=>{
+                return board[cell[0]][cell[1]] === token
+
                 
             })
             if(isWinningCombination){
                 gameActive = false;
-                console.log(`${currentPlayer.name} is the winner!`)
+
+                arr.forEach((cell)=>{
+                    const row = cell[0];
+                    const column = cell[1];
+
+                    const winningCell = document.querySelector(`[data-row="${row}"][data-column="${column}"]`);
+
+                    winningCell.classList.add('winningCell')
+                    roundMessage.textContent = `${currentPlayer.name} is the winner!`
+                    console.log(`${currentPlayer.name} is the winner!`)
+                });
+
+                
             }
         })
-    
-      
-    };
 
+    
+    };
+    
     const checkDraw = () =>{
         const isDraw = board.every((arr)=>{
-           return !arr.includes(null)
+            return !arr.includes(null)
         })
-
+        
         if(isDraw){
+            roundMessage.textContent = `It's a Draw!`
             console.log('Draw')
         }
-
+        
     }
-
+    
     const PlayRound = (row, column) =>{
         if(gameActive){
             gameBoard.dropToken(row, column, getCurrentPlayer().token);
-            
+
+            const btn = document.querySelector(`[data-row = "${row}"][data-column = "${column}"]`);
+            btn.textContent = gameBoard.getBoard()[row][column]
+            console.log(board)
+
             checkWinner(getCurrentPlayer().token);
-            checkDraw()
+            if(gameActive){
+                checkDraw();
+
+                if(gameActive){
+                    switchPlayer();
+                }
+            }
+           
         }
-    }
-
+        
     
+    }
+    
+    buttonCell.forEach((btn)=>{
+        
 
+        btn.addEventListener('click', ()=>{
+            if (btn.textContent !== '') return;
 
+            let row = btn.getAttribute('data-row');
+            let column = btn.getAttribute('data-column')
+         
+            PlayRound(row, column)
+
+        })
+    })
+    
+    
     return {
         getCurrentPlayer,
         switchPlayer,
         PlayRound,
-       
+        
     }
+
 }
+
 
 
 const game = gameController();
 
 
-game.PlayRound(0, 0);
-console.log(gameBoard.getBoard());
-
-game.PlayRound(0, 2);
-console.log(gameBoard.getBoard());
 
 
-game.PlayRound(0, 1);
-console.log(gameBoard.getBoard());
 
-game.PlayRound(1, 0);
-console.log(gameBoard.getBoard());
-
-game.PlayRound(1, 2);
-console.log(gameBoard.getBoard());
-
-game.PlayRound(1, 1);
-console.log(gameBoard.getBoard());
-
-game.PlayRound(2, 0);
-console.log(gameBoard.getBoard());
-
-game.PlayRound(2, 1);
-console.log(gameBoard.getBoard());
-
-game.PlayRound(2, 2);
-console.log(gameBoard.getBoard());
 
 
 
